15aeedbc0c68eb242f4dd961674f908fbc17d533
chihaoyo <chihaoyo@gmail.com>
2019-11-26 02:03:12 +0800

    Update text

--- data/info.js
+++ data/info.js
@@ -1,3 +1,3 @@
 export const BASE_URL = 'https://tjedu.tw/'
 export const SITE_TITLE = '轉型正義教育黑客松'
-export const SITE_DESCRIPTION = '促進轉型正義委員會舉辦「轉型正義教育黑客松」，推動轉型正義普及化，聚集中學教師對轉型正義的關注，向社會各界徵集提案，運用現有素材，設計適合學生的行動方案。'
+export const SITE_DESCRIPTION = '促進轉型正義委員會委託《沃草》於 11/23 舉辦「轉型正義教育黑客松」，聚集中學教師對轉型正義的關注，向社會各界徵集提案，運用現有素材，設計適合學生的行動方案，推動轉型正義普及化。'
