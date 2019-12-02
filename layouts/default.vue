afce26c362ee6b0638bc6c66edc433db515121eb
Charlieeeee <charlieworld2008@gmail.com>
2019-12-02 12:37:33 +0800

    [Modified] add "Microsoft JhengHei"  in font-family (issue #5)

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,4 +7,7 @@
 <style lang="scss">
 @import '~watchout-common-assets/styles/resources';
 @import '~watchout-common-assets/styles/common';
+html, body {
+  font-family: 'Microsoft JhengHei', '微軟正黑體', "SF", "Segoe UI", "Roboto", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+}
 </style>
